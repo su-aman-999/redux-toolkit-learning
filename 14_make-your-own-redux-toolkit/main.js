3d7f929fad7f0c1d2c6b0b0e88144f01813d03f2
Aman Kumar <amankumar746381gmail.com>
2026-04-05 22:55:13 +0530

    14: Make Your Own Redux Toolkit

---
+++ 14_make-your-own-redux-toolkit/main.js
@@ -0,0 +1,25 @@
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { Provider } from "./react-redux";
+import { store } from "./store";
+import { BrowserRouter, Routes, Route, Link } from "react-router";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Headers from "./components/Headers";
+import "./App.css";
+import WishList from "./pages/WishList";
+
+createRoot(document.querySelector("#root")).render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <Headers />
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/cart" element={<Cart />}></Route>
+          <Route path="/wishlist" element={<WishList />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </Provider>,
+);
